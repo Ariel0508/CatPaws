f09d9ffb7be02927a35179a3a4d2c84e0bee36b5
Ariel <sonyt605@gmail.com>
2024-04-08 00:17:35 +0800

    Updates

--- assets/toastMessage-IJTrlXIm.js
+++
@@ -1,1 +0,0 @@
-import{M as o}from"./index-RFYwpk8Q.js";const h=o("toastMessage",{state:()=>({messages:[]}),actions:{toastShow(){setTimeout(()=>{this.messages.shift()},1e3)},clearToast(s){this.messages.splice(s,1)},pushMessage(s){const{style:e="success",title:t,content:a}=s;this.messages.push({style:e,title:t,content:a}),this.toastShow()}}});export{h as u};
